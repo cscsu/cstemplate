556d79bbd53bb267384c19e66ebde247298d105f
Caleb Champlin <caleb.champlin@rams.colostate.edu>
2011-08-17 13:07:26 -0600

    Update schedule support

--- plugins/page_schedule.php
+++ plugins/page_schedule.php
@@ -9,6 +9,7 @@ if ($user->isGuest() != true)
 
 
 
+	$render->assign("admin",1);
 
 	if (!empty($_POST['submit_add']))
 	{
@@ -24,10 +25,10 @@ if ($user->isGuest() != true)
 		}
 	}
 
-	if (!empty($_POST['submit_edit']))
+	else if (!empty($_POST['submit_edit']))
 	{
 		
-
+		$render->assign("edit",true);
 		$errors = doEditItem();
 
 		if (!empty($errors))
@@ -39,10 +40,22 @@ if ($user->isGuest() != true)
 		}
 	}
 
-	if (!empty($_GET['edit']))
+	else if (!empty($_GET['edit']))
 	{
 		$render->assign("edit",true);
 		$errors = doPreEditItem();
+
+		if (!empty($errors))
+
+		{
+
+			$render->assign("errors",$errors);
+
+		}
+	}
+	else if (!empty($_GET['remove']))
+	{
+		$errors = doRemoveItem();
 
 		if (!empty($errors))
 
@@ -57,6 +70,59 @@ if ($user->isGuest() != true)
 else
 {
 	redirect("You must be logged in");
+}
+
+
+function doRemoveItem()
+{
+	global $db;
+	global $config;
+	global $render;
+	global $user;
+	$errors = array();
+
+	$fields = validateFields($_GET, array("remove"=>true));
+
+
+	foreach ($fields as $key => $value)
+
+	{
+
+		if ($value === -1)
+
+			$errors[] = "Missing " . $key . " field";
+
+	}
+
+	if (intval($fields['remove']) <= 0)
+		$errors[] = "Invalid item id";
+	
+	if (!empty($errors))
+
+		return $errors;
+
+	$query = "SELECT * FROM ".$db->prefix."schedule WHERE id='".intval($fields['remove'])."' LIMIT 1";
+	$result = $db->query($query);
+
+	if ($db->num_rows($result) <= 0)
+	{
+		$errors[] = "A entry with that id does not exist";	
+	}
+
+
+	if (empty($errors))
+	{
+		$query = "DELETE FROM ".$db->prefix."resources WHERE sid='".intval($fields['remove'])."'";
+		$db->query($query);
+
+		$query = "DELETE FROM ".$db->prefix."schedule WHERE id='".intval($fields['remove'])."'";
+		$db->query($query);
+
+	
+		redirect("Schedule item deleted", "?page=schedule");
+	
+	}
+	return $errors;
 }
 
 
@@ -81,40 +147,59 @@ function doPreEditItem()
 
 	}
 
-	if (!preg_match("/^[a-zA-Z0-9_]+$/i",$fields['edit']))
-	{
-		$errors[] = "Invalid page name";
-
-
-	}
+	if (intval($fields['edit']) <= 0)
+		$errors[] = "Invalid item id";
 	
 	if (!empty($errors))
 
 		return $errors;
 
-	$query = "SELECT * FROM ".$db->prefix."pages WHERE name='".$db->escape($fields['edit'])."' LIMIT 1";
+	$query = "SELECT * FROM ".$db->prefix."schedule WHERE id='".intval($fields['edit'])."' LIMIT 1";
 	$result = $db->query($query);
 
 	if ($db->num_rows($result) <= 0)
 	{
-		$errors[] = "No such page exists";	
+		$errors[] = "A entry with that id does not exist";	
 	}
+
 
 	if (empty($errors))
 	{
 		
 		$row = $db->fetch_assoc($result);
-		$pagename = $row['name'];
-		$pagetitle = $row['title'];
-		$pagecontent = file_get_contents("templates/content/page_" . $fields['edit'] .".htm");
-		$render->assign("editpage_pagename",$pagename);
 
-
-		$render->assign("editpage_pagetitle",$pagetitle);
-
-
-		$render->assign("editpage_pagecontent",$pagecontent);
-
+		$render->assign("item_date", $row['date']);
+		$render->assign("item_notes", $row['notes']);
+		$render->assign("item_id", $row['id']);
+		$resources = "SELECT * FROM ".$db->prefix."resources WHERE sid=".intval($fields['edit'])." ORDER BY id ASC";
+		$resresult = $db->query($resources);
+		$materials = array();
+		$readings = array();
+		if ($db->num_rows($resresult) > 0)
+		{
+			while ($res = $db->fetch_assoc($resresult))
+			{
+				if (intval($res['type']) == 0)
+				{
+					$curmaterial = array();
+					$curmaterial['id'] = $res['id'];
+					$curmaterial['title'] = $res['title'];
+					$curmaterial['resource'] = $res['resource'];
+					$materials[] = $curmaterial;
+				}
+				else
+				{
+					$curreading = array();
+					$curreading['id'] = $res['id'];
+					$curreading['title'] = $res['title'];
+					$curreading['resource'] = $res['resource'];
+					$readings[] = $curreading;
+				}
+			}
+		}
+		$render->assign("item_materials", $materials);
+		$render->assign("item_readings", $readings);
+		return;
 		
 	}
 		
@@ -131,7 +216,17 @@ function doEditItem()
 	global $user;
 	$errors = array();
 
-	$fields = validateFields($_POST, array("pagename"=>true,"pagetitle"=>true,"pagecontent"=>true));
+
+	$fields = validateFields($_POST, array("itemid"=>true,"itemdate"=>true,
+						"materials_title"=>false,
+						"materials_resource"=>false, 
+						"readings_title"=>false, 
+						"readings_resource"=>false,
+						"materials_titleadd"=>false,
+						"materials_resourceadd"=>false, 
+						"readings_titleadd"=>false, 
+						"readings_resourceadd"=>false,
+						"itemnotes"=>false));
 
 
 	foreach ($fields as $key => $value)
@@ -143,42 +238,123 @@ function doEditItem()
 			$errors[] = "Missing " . $key . " field";
 
 	}
-
-	$render->assign("editpage_pagename",$fields['pagename']);
-
-
-	$render->assign("editpage_pagetitle",$fields['pagetitle']);
+	$render->assign("item_id", $fields['itemid']);
+	$render->assign("item_date",$fields['itemdate']);
+	$render->assign("item_notes",$fields['itemnotes']);
 
 
-	$render->assign("editpage_pagecontent",$fields['pagecontent']);
+
+	$materials = array();
+	$readings = array();
+		
+		if (!empty($fields['materials_title']))
+		{
+			foreach ($fields['materials_title'] as $key => $value)
+			{
+				$id = $key;
+				$title = trim($value);
+				$resource = trim($fields['materials_resource'][$key]);
+				if ($title != "")
+				{
+					$t = array("id"=>$id, "title"=>$title,"resource"=>$resource);
+					$materials[] = $t;
+				}
+			}
+		}
+		
+		if (!empty($fields['readings_title']))
+		{
+		foreach ($fields['readings_title'] as $key => $value)
+		{
+			$id = $key;
+			$title = trim($value);
+			$resource = trim($fields['readings_resource'][$key]);
+			if ($title != "")
+			{
+				$t = array("id"=>$id, "title"=>$title,"resource"=>$resource);
+				$readings[] = $t;
+			}
+		}
+		}
+		if (!empty($fields['materials_titleadd']))
+		{
+		foreach ($fields['materials_titleadd'] as $key => $value)
+		{
+			$id = $key;
+			$title = trim($value);
+			$resource = trim($fields['materials_resourceadd'][$key]);
+			if ($title != "")
+			{
+				$t = array("id"=>$id, "title"=>$title,"resource"=>$resource);
+				$materials[] = $t;
+			}
+		}
+		}
+		if (!empty($fields['readings_titleadd']))
+		{
+		foreach ($fields['readings_titleadd'] as $key => $value)
+		{
+			$id = $key;
+			$title = trim($value);
+			$resource = trim($fields['readings_resourceadd'][$key]);
+			if ($title != "")
+			{
+				$t = array("id"=>$id, "title"=>$title,"resource"=>$resource);
+				$readings[] = $t;
+			}
+		}
+		}
 
 
-	if (!preg_match("/^[a-zA-Z0-9_]+$/i",$fields['pagename']))
+
+
+
+	$render->assign("item_materials",$materials);
+	$render->assign("item_readings",$readings);
+
+
+
+	if (!preg_match("/^[0-1][0-9]\\/[0-3][0-9]\\/20[0-9][0-9]+$/i",$fields['itemdate']))
 	{
-		$errors[] = "Invalid page name";
+		$errors[] = "Invalid date";
 
 
 	}
-	
 	if (!empty($errors))
 		return $errors;
 
-	$query = "SELECT * FROM ".$db->prefix."pages WHERE name='".$db->escape($fields['pagename'])."' LIMIT 1";
+	$query = "SELECT * FROM ".$db->prefix."schedule WHERE id='".intval($fields['itemid'])."' LIMIT 1";
 	$result = $db->query($query);
 
 	if ($db->num_rows($result) <= 0)
 	{
-		$errors[] = "No such page exists";	
+		$errors[] = "A entry with that date does not exist";	
 	}
 
 	if (empty($errors))
 	{
-		
 
-		$query = "UPDATE ".$db->prefix."pages SET title='".$db->escape($fields['pagetitle'])."' WHERE name='".$db->escape($fields['pagename'])."'";
+
+
+		$query = "UPDATE ".$db->prefix."schedule SET date='".$db->escape($fields['itemdate'])."', notes='".$db->escape($fields['itemnotes'])."' WHERE id='".intval($fields['itemid'])."'";
+		$result = $db->query($query);		
+
+		$query = "DELETE FROM ".$db->prefix."resources WHERE sid='".intval($fields['itemid'])."'";
 		$result = $db->query($query);
 
-		file_put_contents("templates/content/page_" . $fields['pagename'] .".htm", $fields['pagecontent']);
+		foreach ($materials as $mat)
+		{
+			$query = "INSERT INTO ".$db->prefix."resources (sid,title,resource,type) VALUES ('".intval($fields['itemid'])."','".$db->escape($mat['title'])."','".$db->escape($mat['resource'])."','0')";
+			$db->query($query);
+		}
+
+		foreach ($readings as $read)
+		{
+			$query = "INSERT INTO ".$db->prefix."resources (sid,title,resource,type) VALUES ('".intval($fields['itemid'])."','".$db->escape($read['title'])."','".$db->escape($read['resource'])."','1')";
+			$db->query($query);
+		}
+		redirect("Schedule item modified","?page=schedule");
+		
 		return null;
 	}
 		
@@ -194,7 +370,7 @@ function doAddItem()
 	global $user;
 	$errors = array();
 
-	$fields = validateFields($_POST, array("itemdate"=>true,"materialstitle"=>false,"materialsresource"=>false, "readingstitle"=>false, "readingsresource"=>false,"itemnotes"=>false));
+	$fields = validateFields($_POST, array("itemdate"=>true,"materials_title"=>false,"materials_resource"=>false, "readings_title"=>false, "readings_resource"=>false,"itemnotes"=>false));
 
 
 	foreach ($fields as $key => $value)
@@ -207,7 +383,12 @@ function doAddItem()
 
 	}
 
-
+	$render->assign("item_date",$fields['itemdate']);
+	$render->assign("item_notes",$fields['itemnotes']);
+	$render->assign("item_materialstitle",$fields['materials_title']);
+	$render->assign("item_materialsresource",$fields['materials_resource']);
+	$render->assign("item_readingstitle",$fields['readings_title']);
+	$render->assign("item_readingsresource",$fields['readings_resource']);
 
 	if (!preg_match("/^[0-1][0-9]\\/[0-3][0-9]\\/20[0-9][0-9]+$/i",$fields['itemdate']))
 	{
@@ -231,10 +412,10 @@ function doAddItem()
 		$materials = array();
 		$readings = array();
 		
-		for ($x = 0; $x < count($fields['materialstitle']); $x++)
+		for ($x = 0; $x < count($fields['materials_title']); $x++)
 		{
-			$title = trim($fields['materialstitle'][$x]);
-			$resource = trim($fields['materialsresource'][$x]);
+			$title = trim($fields['materials_title'][$x]);
+			$resource = trim($fields['materials_resource'][$x]);
 			if ($title != "")
 			{
 				$t = array($title,$resource);
@@ -242,10 +423,10 @@ function doAddItem()
 			}
 		}
 		
-		for ($x = 0; $x < count($fields['readingstitle']); $x++)
+		for ($x = 0; $x < count($fields['readings_title']); $x++)
 		{
-			$title = trim($fields['readingstitle'][$x]);
-			$resource = trim($fields['readingsresource'][$x]);
+			$title = trim($fields['readings_title'][$x]);
+			$resource = trim($fields['readings_resource'][$x]);
 			if ($title != "")
 			{
 				$t = array($title,$resource);
@@ -253,11 +434,24 @@ function doAddItem()
 			}
 		}
 
-		print_r($materials);
-		print_r($readings);
-
 		$query = "INSERT INTO ".$db->prefix."schedule (date,notes) VALUES('".$db->escape($fields['itemdate'])."','".$db->escape($fields['itemnotes'])."')";
 		$result = $db->query($query);
+		$sid = $db->insert_id();
+		
+
+		foreach ($materials as $mat)
+		{
+			$query = "INSERT INTO ".$db->prefix."resources (sid,title,resource,type) VALUES ('".$sid."','".$db->escape($mat[0])."','".$db->escape($mat[1])."','0')";
+			$db->query($query);
+		}
+
+		foreach ($readings as $read)
+		{
+			$query = "INSERT INTO ".$db->prefix."resources (sid,title,resource,type) VALUES ('".$sid."','".$db->escape($read[0])."','".$db->escape($read[1])."','1')";
+			$db->query($query);
+		}
+		redirect("Schedule item added","?page=schedule");
+		
 		return null;
 	}
 		
