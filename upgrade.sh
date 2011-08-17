a8b17cc3e79c23b906571fb7e132efe69e4861bf
Caleb Champlin <caleb.champlin@rams.colostate.edu>
2011-08-17 15:37:47 -0600

    Added upgradge script

---
+++ upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Applying permissions to directory $1"
+chmod 755 $1
+cwd=`pwd`
+cd $1
+echo "Running git Update"
+git pull
+cd $cwd
+echo "Reapplying permissions to directory $1"
+chmod -R 555 $1
+find $1 -type f -name '*.php' -exec chmod 511 {} \;
+chmod -R 711 $1/cache
+chmod -R 711 $1/templates/content
+chmod -R 511 $1/config
