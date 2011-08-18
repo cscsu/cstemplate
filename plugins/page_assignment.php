ccc6f3704fc30c4b3b474512a2025d59a14fa3b9
Caleb Champlin <caleb.champlin@rams.colostate.edu>
2011-08-18 11:59:44 -0600

    Fixed due date information for assignments

--- plugins/page_assignment.php
+++ plugins/page_assignment.php
@@ -11,6 +11,12 @@ if (!empty($_GET['id']))
 	{
 			$assignment  = $db->fetch_assoc($result);
 			$assignment['content'] = file_get_contents("templates/content/assignment_" . $assignment['id'] .".htm");		
+					$dateinfo = strptime($assignment['date'],"%m/%d/%Y");
+		$time = mktime(1,1,1,intval($dateinfo['tm_mon'])+1,$dateinfo['tm_mday'],$dateinfo['tm_year']+1900);
+		$month = date("F", $time);
+		$day = date("j",$time) - $dateinfo['tm_wday'];
+		$assignment['month'] = $month;
+		$assignment['day'] = $day;
 	}
 }
 $render->assign("assignment",$assignment);
